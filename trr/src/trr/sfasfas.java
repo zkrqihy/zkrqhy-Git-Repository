d46d25816a7ba6f2673fc2056263d9b52985c403
zkrsush <zkrsush@..>
2017-02-14 16:46:46 +0800

    vxvxcvxcvxvxc

--- trr/src/trr/sfasfas.java
+++ trr/src/trr/sfasfas.java
@@ -7,5 +7,8 @@ public class sfasfas {
 	///sdfsdfasfasfd
 	public static void main(String[] args){
 		System.out.println("wwwwww");
+		System.out.println("wwwwww");
+		System.out.println("wwwwww");
+		System.out.println("wwwwww");
 	}
 }
