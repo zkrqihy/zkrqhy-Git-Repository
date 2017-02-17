37ec9acaab0d443607190f0798639abca23377f3
zkrqihy <email@zkrqihy@abchinalife.cn>
2017-02-17 14:10:53 +0800

    qhy commit 17 14:10

---
+++ MavenJava/src/main/java/com/sinosoft/App2.java
@@ -0,0 +1,12 @@
+package com.sinosoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public String oo(){
+        return " i am a App oo";	
+       }
+}
---
+++ MavenJava/src/test/java/com/sinosoft/HelloWord/TestApp.java
@@ -0,0 +1,13 @@
+package com.sinosoft.HelloWord;
+
+import org.junit.Test;
+
+import com.sinosoft.App2;
+
+public class TestApp {
+	App2 app = new App2();
+	@Test
+	public void test(){
+		System.out.println(app.oo());
+	}
+}
