920feb4e18f9ad37c89a49bc875fa6983309a4a9
zkrqihy <email@zkrqihy@abchinalife.cn>
2017-02-17 11:26:10 +0800

    qhy commit

---
+++ MavenWeb/src/main/java/com/sinosoft/One.java
@@ -0,0 +1,8 @@
+package com.sinosoft;
+
+public class One {
+
+		public String qq(){
+			return "i am a qq";
+		}
+}
---
+++ MavenWeb/src/test/java/com/sinosoft/TestOne.java
@@ -0,0 +1,12 @@
+package com.sinosoft;
+
+import org.junit.Test;
+
+public class TestOne {
+
+	@Test
+	public void qq(){
+		One one = new One();
+		System.out.println(one.qq());
+	}
+}
