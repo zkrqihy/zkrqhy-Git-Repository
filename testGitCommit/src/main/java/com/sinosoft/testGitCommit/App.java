0a85771a558a1cce5d8b70a4c0cbd25613a87546
zkrqihy <email@zkrqihy@abchinalife.cn>
2017-02-23 15:10:13 +0800

    qhy创建项目

---
+++ testGitCommit/src/main/java/com/sinosoft/testGitCommit/App.java
@@ -0,0 +1,13 @@
+package com.sinosoft.testGitCommit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello QHY" );
+    }
+}
