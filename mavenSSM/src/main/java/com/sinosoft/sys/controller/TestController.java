86d0ffa9908731aa6fa4788acd1563fe69edf7f8
zkrqihy <email@zkrqihy@abchinalife.cn>
2017-02-24 13:48:04 +0800

    qhy2提交

---
+++ mavenSSM/src/main/java/com/sinosoft/sys/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.sinosoft.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class TestController {
+	@RequestMapping("/index")
+	public String indexTest(){
+		return "jsp/index";
+	}
+}
